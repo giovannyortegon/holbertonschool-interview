4a18b081c11aadeaa1490326f257e10d40fc2fab
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-18 22:34:44 -0500

    [FIX] empty box

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -12,7 +12,7 @@ def canUnlockAll(boxes):
 
     for i in range(1, len_box):
         for j in range(0, len_box-1):
-            if len(boxes[0]) == 0 or len(boxes[j]) == 0:
+            if len(boxes[0]) == 0:
                 return False
             if i in boxes[j] and i not in keys:
                 keys.append(i)
