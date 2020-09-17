9d2090e2b7d01d58450b83d261689f60265251ef
Skillhh <ortegon.giovanny@hotmail.com>
2020-09-16 23:44:44 -0500

    [FIX] creating recursion

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,23 +1,38 @@
 #include "palindrome.h"
 /**
- * is_palindrome - create a list and searching
+ * is_palindrome - searching palindrome
  * @n: list toward left
  * Return: 1 Always palindrome
  */
 int is_palindrome(unsigned long n)
 {
-	unsigned long temp, factor, reverse;
+	unsigned long reverse = 0;
+	int palindrome;
 
-	temp = n;
+	palindrome = __palindrome(n, n, reverse);
 
-	while (temp != 0)
+	return (palindrome);
+}
+/**
+  * __palindrome - recursion searched
+  * @n:	remender of operation
+  * @tmp:	copy on original number
+  * @rev:	reverse number
+  * Return: 1 if it is palindrome
+  */
+int __palindrome(unsigned long n, unsigned long tmp, unsigned long rev)
+{
+	if (tmp != 0)
 	{
-		factor = temp % 10;
-		reverse = reverse * 10 + factor;
-		temp /= 10;
+		rev = (rev * 10) + (tmp % 10);
+		tmp /= 10;
+		if (__palindrome(n, tmp, rev))
+			return (1);
+		else
+			return (0);
 	}
 
-	if (reverse == n)
+	if (rev == n)
 		return (1);
 	else
 		return (0);
--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -5,24 +5,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
-
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
 int is_palindrome(unsigned long n);
-int __is_palindrome(listint_t **head);
-int palindrome(listint_t **left, listint_t *right);
+int __palindrome(unsigned long n, unsigned long tmp, unsigned long rev);
 
 #endif /* PALINDROME_H */
